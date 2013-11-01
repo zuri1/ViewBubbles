d9d436461095ab05c0d2b164f4d137b833dd654f
zuri1 <zuri.biringer@gmail.com>
2013-11-01 12:00:54 -0700

    Add 5 bubbles

--- Happy View Bubble Friends/ZMBViewController.m
+++ Happy View Bubble Friends/ZMBViewController.m
@@ -19,11 +19,23 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    UIView *newView = [[UIView alloc]initWithFrame:CGRectMake(20, 20, 40, 40)];
-    newView.backgroundColor = [UIColor blueColor];
-    [self.view addSubview:newView];
-    newView.layer.cornerRadius = 20.f;
-    newView.clipsToBounds = YES;
+    for (int i=0; i<5; i++) {
+    
+        CGFloat superviewWidth = self.view.frame.size.width;
+        CGFloat xPos = superviewWidth/5*i + 5;
+        CGFloat yPos = 20;
+        CGFloat width = superviewWidth/6;
+        CGFloat height = superviewWidth/6;
+        
+        CGRect viewRect = CGRectMake(xPos, yPos, width, height);
+        
+        UIView *newView = [[UIView alloc]initWithFrame:viewRect];
+        newView.backgroundColor = [UIColor blueColor];
+        [self.view addSubview:newView];
+        newView.layer.cornerRadius = width/2;
+        newView.clipsToBounds = YES;
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning
